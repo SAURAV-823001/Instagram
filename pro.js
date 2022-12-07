964da663c936b299c240e06bd37d06164baac5d7
unknown <saurav823001@gmail.com>
2022-12-07 16:26:59 +0530

    print hello javascript

---
+++ pro.js
@@ -0,0 +1,13 @@
+console.log("Hello Javascript");
+console.log("Hello Javascript");
+console.log("Hello Javascript");
+console.log("Hello Javascript");
+console.log("Hello Javascript");
+console.log("Hello Javascript");
+console.log("Hello Javascript");
+console.log("Hello Javascript");
+console.log("Hello Javascript");
+console.log("Hello Javascript");
+console.log("Hello Javascript");
+console.log("Hello Javascript");
+console.log("Hello Javascript");
